1095b6c474477cc625ce8354cb4bdaedca7bdf89
Umer <ashraf1190@gmail.com>
2020-07-20 19:39:04 -0400

    delete old shit

--- src/main/java/contoller/TestData.java
+++ src/main/java/contoller/TestData.java
@@ -3,20 +3,7 @@ package contoller;
 public class TestData {
 
 
-        public static final String FIRSTNAME = "Peter";
-        public static final String LASTNAME = "Parker";
-        public static final String PHONE = "8564495555";
-        public static final String EMAIL = "peter.parker@spiderman.com";
 
-        public static final String STREET1 = "32 Steinway St";
-        public static final String STREET2 = "APT 34";
-        public static final String CITY = "Queens";
-        public static final String STATE = "New York";
-        public static final String POSTALCODE = "11902";
-        public static final String COUNTRY = "MOROCCO";
-
-        public static final String USERNAME = "clarkkent";
-        public static final String PASSWORD = "maryjaneily";
 
 
 
--- src/test/java/SignUp.java
+++ src/test/java/SignUp.java
@@ -9,25 +9,7 @@ public class SignUp extends TestUtilities {
 
     @Test
     public void SignUpWithEmail() throws Exception {
-        LandingPage landingPage = new LandingPage(driver);
-        RegisterPage registerPage = new RegisterPage(driver);
-        // First Step Open Page url
-        landingPage.openLandingPage();
-        // Second Step Click Register
-        landingPage.clickRegister();
-        //Third Step Verify Sign Up page
-        landingPage.verifyRegisterPage();
-        //Fourth Step Enter Personal Info
-        registerPage.enterPersonalInfo(TestData.FIRSTNAME, TestData.LASTNAME, TestData.PHONE, TestData.EMAIL);
-        //Fifth Step Enter Address
-        registerPage.enterAddress(TestData.STREET1, TestData.STREET2, TestData.CITY, TestData.STATE, TestData.POSTALCODE);
-        registerPage.selectCountryDropdown(TestData.COUNTRY);
-        //Sixth Step: Enter New Credentials
-        registerPage.enterNewCredentials(TestData.USERNAME, TestData.PASSWORD, TestData.PASSWORD);
-        //Seventh Step: Click the Submit Button
-        registerPage.submitNewAccount();
-        //Eighth Step: Verify User is Created
-        registerPage.verifySuccess();
+
     }
     @AfterTest
     public void CloseBrowser() {

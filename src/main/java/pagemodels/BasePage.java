a8ca10b3ca2b496d8aa05cc0875a172ecd5cfee4
Umer Ashraf <umer.ashraf@venmo.com>
2020-08-31 17:50:47 -0400

    Completed Flight Booking Automation

--- src/main/java/pagemodels/BasePage.java
+++ src/main/java/pagemodels/BasePage.java
@@ -1,9 +1,14 @@
 package pagemodels;
 
+import jdk.nashorn.internal.ir.BlockLexicalContext;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
@@ -38,7 +43,35 @@ public class BasePage {
         find(locator).isDisplayed();
     }
 
+    protected void waitUntilClick(By locator, int secs)  {
+       (new WebDriverWait(driver, secs))
+                .until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    protected void waitUntilVerify(By locator, int secs)  {
+        (new WebDriverWait(driver, secs))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void pressEnter(By locator) {
+        find(locator).sendKeys(Keys.RETURN);
+    }
+
+    protected void waitUntilTitle(String title, int secs)  {
+        (new WebDriverWait(driver, secs))
+                .until(ExpectedConditions.titleIs(title));
+    }
+
     protected void submit(By locator) {
         find(locator).submit();
+    }
+
+    protected void acceptAlert() {
+        driver.switchTo().alert().accept();
+    }
+
+    protected void findText(String text) {
+        driver.getPageSource().contains(text);
+
     }
 }

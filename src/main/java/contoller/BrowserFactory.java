d317412851ce38f175261e16f1c3e2b0ff3da69e
ashraf1190 <ashraf1190@gmail.com>
2020-09-09 19:32:21 -0400

    Revert "Completed Flight Booking Automation"

--- src/main/java/contoller/BrowserFactory.java
+++ src/main/java/contoller/BrowserFactory.java
@@ -9,7 +9,7 @@ public class BrowserFactory {
 
     public WebDriver createDriver() throws Exception {
         //NOTE : Creating Chrome driver
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver.set(new ChromeDriver());
 
         return driver.get();

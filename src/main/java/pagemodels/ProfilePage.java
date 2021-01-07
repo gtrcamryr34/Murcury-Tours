613dff198cc02e685e483bc5688ad8d06ed3b5e0
bariul28 <bariul.message@gmail.com>
2021-01-06 20:08:25 -0500

    profilePage

--- src/main/java/pagemodels/ProfilePage.java
+++ src/main/java/pagemodels/ProfilePage.java
@@ -5,13 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 import java.sql.Driver;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ProfilePage extends BasePage {
+    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    Date date = new Date();
+
+    String expectedlDate = dateFormat.format(date);
+    private By actualDate = By.xpath("/html/body/div/div[1]/div[1]/div/div/div[2]/div/span");
+
+
 
     public ProfilePage(WebDriver driver) {super(driver);}
+
     String ExpectedWelcomeText ="Hi, Demo User";
     private By WelcomeText = By.xpath("/html/body/div/div[1]/div[1]/div/div/div[1]/div/div[2]/h3");
 
     public void verifiedWelcomeText(){Assert.assertEquals(driver.findElement(WelcomeText),ExpectedWelcomeText);}
+
+    public void verifiedDate(){Assert.assertEquals(driver.findElement(actualDate),expectedlDate);}
+
+
 
 }
--- src/test/java/ProfilePageTest.java
+++ src/test/java/ProfilePageTest.java
@@ -7,6 +7,9 @@ import pagemodels.LandingPage;
 import pagemodels.LoginPage;
 import pagemodels.ProfilePage;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class ProfilePageTest extends BaseScript {
 
     @Test
@@ -15,6 +18,8 @@ public class ProfilePageTest extends BaseScript {
         LoginPage loginPage = new LoginPage(driver);
         AccountPage accountPage = new AccountPage(driver);
         ProfilePage profilePage = new ProfilePage(driver);
+
+
 
         //Step 1 & 2 Open the chrome browser and Type the address "https://phptravels.net/home  " and hit enter
         landingPage.openHomePage();
@@ -27,6 +32,11 @@ public class ProfilePageTest extends BaseScript {
         loginPage.login(TestData.USERNAME, TestData.PASSWORD);
         //Step 7: Verify welcomeText beside profile picture
         profilePage.verifiedWelcomeText();
+        //Step 8: Verify the date of the Profile Page
+        profilePage.verifiedDate();
+
+
+
 
 
 
